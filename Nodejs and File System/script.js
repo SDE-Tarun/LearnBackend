92d744274e75c2b6338fa10513257047cee96b92
Tarun <tarunkumar23j@gmail.com>
2024-10-25 15:28:27 +0530

    Updated Nodejs and File System

---
+++ Nodejs and File System/script.js
@@ -0,0 +1,13 @@
+// Node.js Basics
+// Introduction to Node.js
+// Installing Node.js and npm
+// Working with modules
+// File system operation.
+// Understanding HTTP module.
+
+// npm - jese playstore ya app store hum use krte hai apps download krne ke liye,
+// vese hi hum npm use krte hai code mtlb packages download krne ke liye.
+
+// Nodejs - Programming language, technology, framework, library, software - X mtlb Nahi Hai
+// Nodejs - JS Runtime Environment hai.
+
