fd655492c3f15012846b04245570fbb51c1a760f
Tarun <tarunkumar23j@gmail.com>
2024-10-28 10:48:38 +0530

    File system done

--- Nodejs and File System/script.js
+++ Nodejs and File System/script.js
@@ -1,13 +1,94 @@
-// Node.js Basics
-// Introduction to Node.js
-// Installing Node.js and npm
-// Working with modules
-// File system operation.
-// Understanding HTTP module.
+// Aapne fs module ko bahar nikala hai node se aur apne is constant mein move krlia hai.
+const fs = require('fs');
 
-// npm - jese playstore ya app store hum use krte hai apps download krne ke liye,
-// vese hi hum npm use krte hai code mtlb packages download krne ke liye.
+// writeFile
+// appendFile
+// copyFile
+// rename
+// unlink
 
-// Nodejs - Programming language, technology, framework, library, software - X mtlb Nahi Hai
-// Nodejs - JS Runtime Environment hai.
+// fs.writeFile("./newFolder/hey.txt", "hey how are you!", function (err) {
+//     if(err){
+//         console.error(err);
+//     }
+//     else{
+//         console.log("done");
+        
+//     }
+// })
+
+// fs.appendFile("hey.txt", " What's going on!", function (err) {
+//     if(err){
+//         console.error(err);
+//     }
+//     else{
+//         console.log("done");
+        
+//     }
+// })
+
+// fs.rename("hey.txt", "hello.txt", function (err) {
+//     if(err) console.error(err.message);
+//     else console.log("rename done");
+// })
+
+// fs.copyFile("hello.txt", "./copy/copy.txt", function (err){
+//     if(err) console.error(err.message);
+//     else console.log("copy done");
+// })
+
+// fs.unlink("hello.txt", function (err){
+//     if(err) console.error(err.message);
+//     else console.log("removed done");
+// })  
+
+// fs.rmdir("./newFolder", {recursive: true} , function (err){
+//     if(err) console.error(err);
+//     else console.log("removed directory");
+// })
+
+// fs.rm("./newFolder/index.html", {recursive: true} , function (err){
+//     if(err) console.error(err);
+//     else console.log("removed directory");
+// })
+
+// fs.mkdir("./newFolder", function (err) {
+//     if(err) console.error(err);
+//     else console.log('Folder created');
+// })
+
+// fs.readdir("./newFolder", function (err, files) {
+//     if (err) console.error('Error reading folder:', err);
+//     else console.log('Files in folder:', files);
+// })
+
+// fs.readFile("./newFolder/hey.txt", "utf-8", function (err, data) {
+//     if (err) console.error('Error reading file:', err);
+//     else console.log('Files content:', data);
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
